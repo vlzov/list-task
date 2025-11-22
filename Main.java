512577313025e10550d22b14b02322c849ca1f9e
Vladimir <vlzov36@gmail.com>
2025-11-22 12:00:08 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -14,7 +14,6 @@ public class Main {
 
         while(true) {
             System.out.println("Выберите операцию\n");
-            System.out.println("0. Выход из программы");
             System.out.println("1. Добавить дело");
             System.out.println("2. Показать дела");
             System.out.println("3. Удалить дело по номеру");
@@ -38,13 +37,29 @@ public class Main {
                     break;
                 
                 case 2:
+                    if(casesList.isEmpty()) {
+                        System.out.println("Список дел пуст!");
+                        break;
+                    }
+
                     showCases(casesList);
                     System.out.println();
                     break;
 
                 case 3:
+                    if(casesList.isEmpty()) {
+                        System.out.println("Список дел пуст!");
+                        break;
+                    }
+
                     System.out.print("Введите номер для удаления: ");
                     choice = scanner.nextInt();
+                    scanner.nextLine();
+
+                    if(choice < 1 || choice > casesList.size()) {
+                        System.out.println("Ошбика: дела с индексом " + choice + " не существует!");
+                        break;
+                    }
 
                     casesList.remove(choice - 1);
                     System.out.println("Удалено!");
@@ -54,10 +69,21 @@ public class Main {
                     break;
 
                 case 4:
+                    if(casesList.isEmpty()) {
+                        System.out.println("Список дел пуст!");
+                        break;
+                    }
+
                     System.out.print("Введите название для удаления: ");
                     caseInput = scanner.nextLine();
 
-                    casesList.remove(caseInput);
+                    boolean wasRemoved = casesList.remove(caseInput);
+
+                    if(!wasRemoved) {
+                        System.out.println("Ошибка: дела с названием " + caseInput + " не сущетсвует!");
+                        break;
+                    }
+
                     System.out.println("Удалено!");
                     
                     showCases(casesList);
